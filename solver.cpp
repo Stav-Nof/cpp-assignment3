a7aa538c9e42516acee3092b793b86017e317c59
jordan <jordantangy@gmail.com>
2020-05-03 17:24:29 +0300

    completing implementation of RV class

--- solver.cpp
+++ solver.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <complex>
+#include <stdexcept>
 #include "solver.hpp"
 
 
@@ -34,12 +35,39 @@ solver::RealVariable& solver::RealVariable::operator==(double num){
     return *this;
 }
 solver::RealVariable& solver::RealVariable::operator==(RealVariable& other){
+    this->_a -= other._a;
+    this->_b -= other._b;
+    this->_c -= other._c;
     return *this;
 }
 solver::RealVariable& solver::RealVariable::operator^(double num){
+    
+    if(num > 2 || num < 0){
+        throw "OUT OF RANGE !";
+    }
+    else if(num == 0){
+        this->_a = 0;
+        this->_b = 0;
+        this->_c = 1;
+    }
+    else if(num == 1){
+        return *this;
+    }
+    else {
+        this->_a *=this->_a;
+        this->_b =2*(this->_a*this->_b);
+        this->_c *=this->_c;
+    }
     return *this;
 }
 solver::RealVariable& solver::RealVariable::operator/(double num){
+    
+    if(num == 0){
+        throw std::runtime_error("Math error: Attempted to divide by zero\n");
+    }
+    this->_a /= num;
+    this->_b /= num;
+    this->_c /= num;
     return *this;
 }
 solver::RealVariable& solver::operator*(double num, RealVariable& x){
